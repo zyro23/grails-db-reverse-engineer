66736d7e2f0f31116a99f710262868a187e3d5da
Burt Beckwith <burt@burtbeckwith.com>
2011-11-10 15:53:05 -0500

    more Grails 2 updates, and not depending on Hibernate plugin, just its jars

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,11 +1,9 @@
-grails.project.class.dir = 'target/classes'
-grails.project.test.class.dir = 'target/test-classes'
-grails.project.test.reports.dir = 'target/test-reports'
-grails.project.docs.output.dir = 'docs' // docs are checked into gh-pages branch
+grails.project.work.dir = 'target'
+grails.project.docs.output.dir = 'docs/manual' // for backwards-compatibility, the docs are checked into gh-pages branch
 
 grails.project.dependency.resolution = {
-	inherits 'global'
 
+	inherits 'global'
 	log 'warn'
 
 	repositories {
@@ -17,17 +15,17 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		compile('org.hibernate:hibernate-tools:3.2.4.GA') {
-			transitive = false
-		}
-		compile('freemarker:freemarker:2.3.8') {
-			transitive = false
-		}
-		compile('org.beanshell:bsh:2.0b4') {
-			transitive = false
-		}
-		compile('org.hibernate:jtidy:r8-20060801') {
-			transitive = false
-		}
+		compile('org.hibernate:hibernate-tools:3.2.4.GA') { transitive = false }
+
+		compile('org.hibernate:hibernate-core:3.3.1.GA') { transitive = false }
+		compile('dom4j:dom4j:1.6.1') { transitive = false }
+
+		compile('freemarker:freemarker:2.3.8') { transitive = false }
+		compile('org.beanshell:bsh:2.0b4') { transitive = false }
+		compile('org.hibernate:jtidy:r8-20060801') { transitive = false }
+	}
+
+	plugins {
+		build(':release:1.0.0.RC3') { export = false }
 	}
 }
