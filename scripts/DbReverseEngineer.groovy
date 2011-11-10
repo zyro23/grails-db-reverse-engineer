4ea02dfc69a8ae5b04398a89ebd3f216ddf6b3a1
Burt Beckwith <burt@burtbeckwith.com>
2011-11-10 14:37:44 -0500

    better error handling, and switched from ant.echo to event('StatusUpdate', msg)

--- scripts/DbReverseEngineer.groovy
+++ scripts/DbReverseEngineer.groovy
@@ -13,78 +13,88 @@
  * limitations under the License.
  */
 
+import grails.util.GrailsUtil
+
 includeTargets << grailsScript('_GrailsBootstrap')
+
+USAGE = 'grails db-reverse-engineer'
 
 /**
  * @author <a href='mailto:burt@burtbeckwith.com'>Burt Beckwith</a>
  */
 target(dbReverseEngineer: 'Reverse-engineers a database and creates domain classes') {
-	depends(packageApp, loadApp)
+	depends packageApp, loadApp
 
-	createConfig()
-	def dsConfig = config.dataSource
+	try {
+		createConfig()
+		def dsConfig = config.dataSource
 
-	def reenigne = classLoader.loadClass('grails.plugin.reveng.Reenigne').newInstance()
-	reenigne.grailsConfig = config
-	reenigne.driverClass = dsConfig.driverClassName ?: 'org.hsqldb.jdbcDriver' // 'org.h2.Driver'
-	reenigne.password = dsConfig.password ?: ''
-	reenigne.username = dsConfig.username ?: 'sa'
-	reenigne.url = dsConfig.url ?: 'jdbc:hsqldb:mem:testDB' // 'jdbc:h2:mem:testDB'
-	if (dsConfig.dialect instanceof String) {
-		reenigne.dialect = dsConfig.dialect
+		def reenigne = classLoader.loadClass('grails.plugin.reveng.Reenigne').newInstance()
+		reenigne.grailsConfig = config
+		reenigne.driverClass = dsConfig.driverClassName ?: 'org.h2.Driver'
+		reenigne.password = dsConfig.password ?: ''
+		reenigne.username = dsConfig.username ?: 'sa'
+		reenigne.url = dsConfig.url ?: 'jdbc:h2:mem:testDB'
+		if (dsConfig.dialect instanceof String) {
+			reenigne.dialect = dsConfig.dialect
+		}
+		else if (dsConfig.dialect instanceof Class) {
+			reenigne.dialect = dsConfig.dialect.name
+		}
+
+		def revengConfig = config.grails.plugin.reveng
+		reenigne.packageName = revengConfig.packageName ?: metadata['app.name']
+		reenigne.destDir = new File(basedir, revengConfig.destDir ?: 'grails-app/domain')
+		if (revengConfig.defaultSchema) {
+			reenigne.defaultSchema = revengConfig.defaultSchema
+		}
+		if (revengConfig.defaultCatalog) {
+			reenigne.defaultCatalog = revengConfig.defaultCatalog
+		}
+		if (revengConfig.overwriteExisting instanceof Boolean) {
+			reenigne.overwrite = revengConfig.overwriteExisting
+		}
+
+		def strategy = reenigne.reverseEngineeringStrategy
+
+		revengConfig.versionColumns.each { table, column -> strategy.addVersionColumn table, column }
+
+		revengConfig.manyToManyTables.each { table -> strategy.addManyToManyTable table }
+
+		revengConfig.manyToManyBelongsTos.each { manyTable, belongsTable -> strategy.setManyToManyBelongsTo manyTable, belongsTable }
+
+		revengConfig.includeTables.each { table -> strategy.addIncludeTable table }
+
+		revengConfig.includeTableRegexes.each { pattern -> strategy.addIncludeTableRegex pattern }
+
+		revengConfig.includeTableAntPatterns.each { pattern -> strategy.addIncludeTableAntPattern pattern }
+
+		revengConfig.excludeTables.each { table -> strategy.addExcludeTable table }
+
+		revengConfig.excludeTableRegexes.each { pattern -> strategy.addExcludeTableRegex pattern }
+
+		revengConfig.excludeTableAntPatterns.each { pattern -> strategy.addExcludeTableAntPattern pattern }
+
+		revengConfig.excludeColumns.each { table, columns -> strategy.addExcludeColumns table, columns }
+
+		revengConfig.excludeColumnRegexes.each { table, patterns -> strategy.addExcludeColumnRegexes table, patterns }
+
+		revengConfig.excludeColumnAntPatterns.each { table, patterns -> strategy.addExcludeColumnAntPatterns table, patterns }
+
+		revengConfig.mappedManyToManyTables.each { table -> strategy.addMappedManyToManyTable table }
+
+		if (revengConfig.alwaysMapManyToManyTables instanceof Boolean) {
+			strategy.alwaysMapManyToManyTables = revengConfig.alwaysMapManyToManyTables
+		}
+
+		event('StatusUpdate', ["Starting database reverse engineering, connecting to '$reenigne.url' as '$reenigne.username' ..."])
+		reenigne.execute()
+		event('StatusUpdate', ['Finished database reverse engineering'])
 	}
-	else if (dsConfig.dialect instanceof Class) {
-		reenigne.dialect = dsConfig.dialect.name
+	catch (e) {
+		GrailsUtil.deepSanitize e
+		throw e
 	}
-
-	def revengConfig = config.grails.plugin.reveng
-	reenigne.packageName = revengConfig.packageName ?: metadata['app.name']
-	reenigne.destDir = new File(basedir, revengConfig.destDir ?: 'grails-app/domain')
-	if (revengConfig.defaultSchema) {
-		reenigne.defaultSchema = revengConfig.defaultSchema
-	}
-	if (revengConfig.defaultCatalog) {
-		reenigne.defaultCatalog = revengConfig.defaultCatalog
-	}
-	if (revengConfig.overwriteExisting instanceof Boolean) {
-		reenigne.overwrite = revengConfig.overwriteExisting
-	}
-
-	def strategy = reenigne.reverseEngineeringStrategy
-
-	revengConfig.versionColumns.each { table, column -> strategy.addVersionColumn table, column }
-
-	revengConfig.manyToManyTables.each { table -> strategy.addManyToManyTable table }
-
-	revengConfig.manyToManyBelongsTos.each { manyTable, belongsTable -> strategy.setManyToManyBelongsTo manyTable, belongsTable }
-
-	revengConfig.includeTables.each { table -> strategy.addIncludeTable table }
-
-	revengConfig.includeTableRegexes.each { pattern -> strategy.addIncludeTableRegex pattern }
-
-	revengConfig.includeTableAntPatterns.each { pattern -> strategy.addIncludeTableAntPattern pattern }
-
-	revengConfig.excludeTables.each { table -> strategy.addExcludeTable table }
-
-	revengConfig.excludeTableRegexes.each { pattern -> strategy.addExcludeTableRegex pattern }
-
-	revengConfig.excludeTableAntPatterns.each { pattern -> strategy.addExcludeTableAntPattern pattern }
-
-	revengConfig.excludeColumns.each { table, columns -> strategy.addExcludeColumns table, columns }
-
-	revengConfig.excludeColumnRegexes.each { table, patterns -> strategy.addExcludeColumnRegexes table, patterns }
-
-	revengConfig.excludeColumnAntPatterns.each { table, patterns -> strategy.addExcludeColumnAntPatterns table, patterns }
-
-	revengConfig.mappedManyToManyTables.each { table -> strategy.addMappedManyToManyTable table }
-
-	if (revengConfig.alwaysMapManyToManyTables instanceof Boolean) {
-		strategy.alwaysMapManyToManyTables = revengConfig.alwaysMapManyToManyTables
-	}
-
-	ant.echo message: "Starting database reverse engineering, connecting to '$reenigne.url' as '$reenigne.username' ..."
-	reenigne.execute()
-	ant.echo message: 'Finished database reverse engineering'
 }
 
 setDefaultTarget dbReverseEngineer
